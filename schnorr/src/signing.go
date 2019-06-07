3dd6c1cca82870dfc894627f532f9d53b43ffbc2
Rebekah <rebekahmercer@hotmail.co.uk>
2019-06-07 14:22:40 -0700

    removing unecessary code

--- schnorr/src/signing.go
+++ schnorr/src/signing.go
@@ -17,8 +17,22 @@ To sign a message m:
 
 func main () {
 
+	// read in message from stdio
+	m := 
 	// scalars from here
-	bn256.Order
+	order := bn256.Order
+	// Choose random k from F_order
+	k := bn256.RandScalar()
+	// Let r = g^k 
+	r := bn256.BaseMult(k)
+	// Let e = H ( r ∥ m )
+	e := Sha256.Digest(r)
+	e = Sha256.Digest(m)
+	// Let s = k − x e (mod order)
+	s := 
+	// print signature (s, e) to stdio 
+	// (change these to rpcs using metamask?)
+
 
 }
 
